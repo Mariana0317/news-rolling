14b780be2eb7b9ba980a2d469f3f95e7f9e0f9ca
Mariana0317 <mariana.granara@gmail.com>
2020-09-11 16:37:48 -0300

    commit para cambiar de rama

--- src/components/commons/Footer.js
+++ src/components/commons/Footer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
+
 
 const Footer = () => {
     return (
@@ -9,7 +12,7 @@ const Footer = () => {
   <Container className="d-flex justify-content-between">
       <section className=""><h1>Rolling News</h1></section>
       <section className=""><p>jdsk fjskfjifi skjfsf sljf sfs jlskfjsif s </p></section>
-      <section className=""><p>dfjfj f dsjf</p></section>
+      <section className=""><p><FontAwesomeIcon icon={faFacebook} /></p> <p><FontAwesomeIcon icon={faInstagramSquare} /></p></section>
     
   </Container>
 </Jumbotron>
