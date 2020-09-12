c4ae14516aaf627d46c37a0fdab9a91abb24bbf4
Mariana0317 <mariana.granara@gmail.com>
2020-09-12 20:31:33 -0300

    aqui envio el footer

--- src/components/commons/Header.js
+++ src/components/commons/Header.js
@@ -16,18 +16,18 @@ const Header = () => {
     <div className="">
       <h1 className="text-center my-5">Rolling News</h1>
 
-      <Navbar bg="light" expand="lg" className="d-flex justify-content-around py-3">
-        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+      <Navbar bg="light" expand="lg" className=" ">
+        <Navbar.Brand href="#">Logo</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto ">
-            <Nav.Link className="mx-4">
+          <Nav className="mr-auto w-100 d-flex justify-content-between">
+            <Nav.Link className="ml-3">
               <FontAwesomeIcon icon={faHome} />
             </Nav.Link>
-            <Nav.Link className="mx-5">Actualidad</Nav.Link>
-            <Nav.Link className="mx-5">Espectaculos</Nav.Link>
-            <Nav.Link className="mx-5">Tecnologia</Nav.Link>
-            <Nav.Link className="mx-5">Deportes</Nav.Link>
+            <Nav.Link className="">Actualidad</Nav.Link>
+            <Nav.Link className="">Espectaculos</Nav.Link>
+            <Nav.Link className="">Tecnologia</Nav.Link>
+            <Nav.Link className="">Deportes</Nav.Link>
             <NavDropdown title="+ Noticias" id="categoria">
               <NavDropdown.Item>Politica</NavDropdown.Item>
               <NavDropdown.Item>Economia</NavDropdown.Item>
@@ -38,39 +38,35 @@ const Header = () => {
                 <FontAwesomeIcon icon={faCameraRetro} />
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link className="mx-5">
+            <Nav.Link className="">
               <FontAwesomeIcon icon={faUser} />
             </Nav.Link>
-            <Nav.Link  className="mx-5">
-              <Button variant="outline-dark">Suscribirse</Button>
+            <Nav.Link className="">
+              <Button size="sm" variant="outline-dark">Suscribirse</Button>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        bg="dark"
-        variant="dark"
-        className=" d-flex justify-content-center"
-      >
-        <Navbar.Brand href="#home">Logo</Navbar.Brand>
+      <Navbar expand="lg" bg="dark" variant="dark" className=" ">
+        <Navbar.Brand href="#">Logo</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#features" className="mx-5">
+          <Nav className="mr-auto w-100 d-flex justify-content-around">
+            <Nav.Link href="#" className="">
               <FontAwesomeIcon icon={faHome} />
             </Nav.Link>
-            <Nav.Link href="#pricing" className="mx-5">
+            <Nav.Link href="#pricing" className="">
               Lista de Noticias
             </Nav.Link>
-            <Nav.Link href="#pricing" className="mx-5">
+            <Nav.Link href="#pricing" className="">
               Lista de Categorias
             </Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link eventKey={2} href="#memes">
-              <Button variant="success">Cerrar Sesion</Button>
+            <Nav.Link href="#">
+              <Button variant="success" size="sm">
+                Cerrar Sesion
+              </Button>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
