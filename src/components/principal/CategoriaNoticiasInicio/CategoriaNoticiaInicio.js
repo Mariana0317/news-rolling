ab205631c5cef039c7dd512fdf97ca7063fd9d6d
Leonardo Palavecino <leo43161301gmail.com>
2020-09-14 20:48:52 -0300

    Maquetado de categorias de noticias en inicio realizado

---
+++ src/components/principal/CategoriaNoticiasInicio/CategoriaNoticiaInicio.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+const CategoriaNoticiaInicio = () => {
+  return (
+    <ListGroup.Item action>
+      <div className="d-flex justify-content-between">
+        <div className="col-7 p-0">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">
+              El Partido Comunista de Venezuela denunció que está recibiendo
+              “ataques desproporcionados" del régimen de Nicolás Maduro
+            </h5>
+          </div>
+          <p className="mb-1">
+            Sus líderes acusan al dictador de llevar adelante una política
+            “liberal y entreguista” y se unieron a los partidos rebeldes del
+            chavismo que armaron la lista Alternativa Popular Revolucionaria
+            para las elecciones parlamentarias
+          </p>
+          <small>Fecha de Publicacion: 11/8/2020</small>
+        </div>
+        <div className="col-4 p-0 h-100 my-auto">
+          <img
+            src={process.env.PUBLIC_URL + "/img/noticia1.jpg"}
+            alt="noticia1"
+            className="img-fluid rounded"
+          />
+        </div>
+      </div>
+    </ListGroup.Item>
+  );
+};
+
+export default CategoriaNoticiaInicio;
--- src/components/principal/Inicio.js
+++ src/components/principal/Inicio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardClima from './CardClima/CardClima';
+import CategoriasNoticiasInicio from './CategoriaNoticiasInicio/CategoriasNoticiasInicio';
 import MonedasValores from './MonedaApi/MonedasValores';
 import NoticiasDestacadas2 from './noticiasDestacadas/NoticiasDestacadas2';
 
@@ -10,8 +11,8 @@ const Inicio = () => {
             <MonedasValores></MonedasValores>
             <NoticiasDestacadas2></NoticiasDestacadas2>
             <section className="container d-flex">
-                <div className="col-9 border border-dark">
-                    fsdfsfsdfsdfsdf
+                <div className="col-9 px-1">
+                    <CategoriasNoticiasInicio></CategoriasNoticiasInicio>
                 </div>
                 <div className="col-3 border border-dark">
 
