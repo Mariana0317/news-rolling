57064e718ed34b2aec4cdc4f97b757a27b5fb867
Leonardo Palavecino <leo43161301gmail.com>
2020-09-14 08:25:55 -0300

    Api aplicado en componente de valores de moneda

--- src/components/principal/MonedaApi/MonedaValor.js
+++ src/components/principal/MonedaApi/MonedaValor.js
@@ -1,20 +1,21 @@
 import React from "react";
 
-const MonedaValor = () => {
+const MonedaValor = (props) => {
+  console.log(props.moneda);
   return (
     <div className="d-flex flex-column px-2 py-1 col-3">
       <div className="border border-dark">
         <div className="text-center border-bottom border-dark w-100">
-          <h4>Dolar Oficial</h4>
+          <h4>{props.moneda.nombre}</h4>
         </div>
         <div className="d-flex justify-content-between">
           <div className="text-center w-50">
-            <h5>Venta</h5>
-            <h5>$ 0,0</h5>
+            <h5>Compra</h5>
+            <h6>$ {props.moneda.compra}</h6>
           </div>
           <div className="text-center w-50">
             <h5>Venta</h5>
-            <h5>$ 0,0</h5>
+            <h6>$ {props.moneda.venta}</h6>
           </div>
         </div>
       </div>
