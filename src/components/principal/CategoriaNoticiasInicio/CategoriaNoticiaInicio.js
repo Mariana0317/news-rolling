763f49ae0dad00ccbb585979f0194f291228baec
Leonardo Palavecino <leo43161301gmail.com>
2020-09-21 11:31:24 -0300

    se actualizo el componente categorianoticiainicio para que se vaya a detalle noticia de la noticia elegida

--- src/components/principal/CategoriaNoticiasInicio/CategoriaNoticiaInicio.js
+++ src/components/principal/CategoriaNoticiasInicio/CategoriaNoticiaInicio.js
@@ -1,30 +1,38 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
+import { Link } from "react-router-dom";
 
 const CategoriaNoticiaInicio = (props) => {
   return (
-    <ListGroup.Item action className="border rounded my-2 shadow-sm">
-      <div className="d-flex justify-content-between">
-        <div className="col-md-7 col-12 p-0">
-          <div className="d-flex w-100 justify-content-between">
-            <h5 className="mb-1">
+    <Link to={`/detalle-noticia/${props.noticia !== undefined ? props.noticia._id : null}`}>
+      <ListGroup.Item action className="border rounded my-2 shadow-sm">
+        <div className="d-flex justify-content-between">
+          <div className="col-md-7 col-12 p-0">
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">
+                {props.noticia !== undefined ? props.noticia.titulo : null}
+              </h5>
+            </div>
+            <p className="mb-1">
               {props.noticia !== undefined ? props.noticia.titulo : null}
-            </h5>
+            </p>
+            <small>
+              Fecha de Publicacion:{" "}
+              {props.noticia !== undefined ? props.noticia.fecha : null}
+            </small>
           </div>
-          <p className="mb-1">
-          {props.noticia !== undefined ? props.noticia.titulo : null}
-          </p>
-          <small>Fecha de Publicacion: {props.noticia !== undefined ? props.noticia.fecha : null}</small>
+          <div className="col-md-4 d-none d-md-block p-0 h-100 my-auto">
+            <img
+              src={
+                props.noticia !== undefined ? props.noticia.imgPrincipal : null
+              }
+              alt="noticia1"
+              className="img-fluid rounded"
+            />
+          </div>
         </div>
-        <div className="col-md-4 d-none d-md-block p-0 h-100 my-auto">
-          <img
-            src={props.noticia !== undefined ? props.noticia.imgPrincipal: null }
-            alt="noticia1"
-            className="img-fluid rounded"
-          />
-        </div>
-      </div>
-    </ListGroup.Item>
+      </ListGroup.Item>
+    </Link>
   );
 };
 
