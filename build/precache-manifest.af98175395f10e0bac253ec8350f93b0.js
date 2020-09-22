dab6cbc2f2a0e61f6b38a55ad1484ebca973cf18
Franco Luna <francooluna1@hotmail.com>
2020-09-22 04:41:16 -0300

    Se quito build de gitignore

---
+++ build/precache-manifest.af98175395f10e0bac253ec8350f93b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610707b3b991aa3d192ea60069d8a055",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ef41a911da5391e8e42",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "101c959bc18538e179e0",
+    "url": "/static/css/main.94129f66.chunk.css"
+  },
+  {
+    "revision": "3ef41a911da5391e8e42",
+    "url": "/static/js/2.1e537ecb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1e537ecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "101c959bc18538e179e0",
+    "url": "/static/js/main.85613c8b.chunk.js"
+  },
+  {
+    "revision": "efece339381b8fe001eb",
+    "url": "/static/js/runtime-main.48ed5e32.js"
+  }
+]);
