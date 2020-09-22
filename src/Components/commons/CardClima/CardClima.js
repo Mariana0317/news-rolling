4e1267724d2607feaaef60472221138cb483ae28
Franco Luna <francooluna1@hotmail.com>
2020-09-22 01:00:13 -0300

    Se cambio el tamaño del logo

--- src/Components/commons/CardClima/CardClima.js
+++ src/Components/commons/CardClima/CardClima.js
@@ -45,14 +45,14 @@ const CardClima = () => {
     <div id="top">
     <div className="text-light card-clima col-12 justify-content-between d-none d-lg-block">
       <div className="w-100 h-100 d-flex">
-        <div className="col-4 d-column justify-content-start align-items-center">
-          <h2 className="display-5 text-center text-light" id="hours">...</h2>
+        <div className="col-4 d-column justify-content-start align-self-center">
+          <h2 className="text-center text-light" id="hours">...</h2>
           <p className="text-center mb-1" id="date">Cargando...</p>
         </div>
         <div className="wheather col-4 d-flex flex-column justify-content-around align-items-center">
-          <p className="m-0 mt-2">{located.city}</p>
+          <p className="m-0">{located.city}</p>
         </div>
-        <div className="wheather col-4 d-flex flex-column justify-content-start align-items-center">
+        <div className="wheather col-4 d-flex justify-content-center align-items-center">
           {weather.weather ? (
             <img
               src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
--- src/components/principal/MonedaApi/MonedaValor.js
+++ src/components/principal/MonedaApi/MonedaValor.js
@@ -3,9 +3,9 @@ import React from "react";
 const MonedaValor = (props) => {
   console.log(props.moneda);
   return (
-    <div className="d-md-flex flex-column px-2 py-1 col-3 d-none mt-5 mb-3" >
+    <div className="d-lg-flex flex-column px-2 py-1 col-3 d-none d-lg-block mt-5 mb-3" >
       <div className="border border-dark">
-        <div className="moneda text-center border-bottom border-dark w-100">
+        <div className="moneda text-center border-bottom border-dark w-100 p-1">
           <h5><strong>{props.moneda.nombre}</strong></h5>
         </div>
         <div className="d-flex justify-content-between">
