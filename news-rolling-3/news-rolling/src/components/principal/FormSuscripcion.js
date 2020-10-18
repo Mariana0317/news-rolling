670173f0066176a910bb8f9fdc112132cae9ad36
Leonardo Palavecino <leo43161301gmail.com>
2020-10-17 21:04:34 -0300

    Se actualizo el funcionamiento de el formulario de suscripcion de manera local

--- news-rolling-3/news-rolling/src/components/principal/FormSuscripcion.js
+++ news-rolling-3/news-rolling/src/components/principal/FormSuscripcion.js
@@ -12,53 +12,91 @@ const FormSuscripcion = (props) => {
   const [error, setError] = useState(false);
   const [mensajeError, setMensajeError] = useState("");
 
-
   const [formulario, setFormulario] = useState({
-    nombreYApellido: "",
+    nombre: "",
     email: "",
     codigoPostal: "",
     localidad: "",
     direccion: "",
+    contrasenia: "",
+    reContrasenia: "",
   });
 
   let {
-    nombreYApellido,
+    nombre,
     email,
     codigoPostal,
     localidad,
     direccion,
+    contrasenia,
+    reContrasenia,
   } = formulario;
 
   const handleChange = (e) => {
     setFormulario({
       ...formulario,
       [e.target.name]: e.target.value,
-    })
-  }
+    });
+  };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const expresion = /\w+@\w+\.[a-z]/;
     //Validar los campos
-    console.log()
-    if (nombreYApellido.trim() !== "" &&
+    console.log();
+    if (
+      nombre.trim() !== "" &&
       email.trim() !== "" &&
       codigoPostal.trim() !== "" &&
       localidad.trim() !== "" &&
-      direccion.trim() !== "") {
+      direccion.trim() !== ""
+    ) {
       if (expresion.test(email)) {
-        Swal.fire(
-          "Datos registrados!",
-          "Pronto nos pondremos en contacto con usted para finalizar la suscripción.",
-          "success"
-        );
-        props.history.push("/");
+        if (contrasenia === reContrasenia) {
+          try {
+            const consulta = await fetch(`http://localhost:4000/users/singup`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                nombre,
+                email,
+                direccion,
+                localidad,
+                codigoPostal,
+                contrasenia,
+                activo: false,
+                logueado: false,
+                usuario: 2,
+              }),
+            });
+            const resultado = await consulta.json();
+            console.log(consulta);
+            console.log(resultado);
+            if (consulta.status === 201) {
+              Swal.fire(
+                "Datos registrados!",
+                "Pronto nos pondremos en contacto con usted para finalizar la suscripción.",
+                "success"
+              );
+              props.history.push("/");
+            } else {
+              setError(true);
+              setMensajeError(resultado.mensaje);
+            }
+          } catch (error) {
+            console.log(error);
+          }
+        } else {
+          setError(true);
+          setMensajeError("Las contraseñas no coinciden");
+        }
       } else {
-        setError(true)
+        setError(true);
         setMensajeError("Email inválido");
         return;
       }
-
     } else {
       setError(true);
       setMensajeError("Todos los campos son obligatorios");
@@ -68,62 +106,96 @@ const FormSuscripcion = (props) => {
   return (
     <div className="formularios">
       <Container className="text-dark rounded">
-        <h2 className="text-center mt-5 text-danger">Formulario de suscripción</h2>
+        <h2 className="text-center mt-5 text-danger">
+          Formulario de suscripción
+        </h2>
         <Form className="my-4" onSubmit={handleSubmit}>
-            <Form.Group as={Col} controlId="nombreYApellido">
-              <Form.Label className="formLetraCategoria"><strong>Nombre completo *</strong></Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Ingresa tu apellido y nombre"
-                name="nombreYApellido"
-                onChange={handleChange}
-              />
-            </Form.Group>
+          <Form.Group as={Col} controlId="nombreYApellido">
+            <Form.Label className="formLetraCategoria">
+              <strong>Nombre completo *</strong>
+            </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ingresa tu apellido y nombre"
+              name="nombre"
+              onChange={handleChange}
+            />
+          </Form.Group>
 
-            <Form.Group as={Col} controlId="email">
-              <Form.Label className="formLetraCategoria"><strong>Correo electrónico *</strong></Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Ingresa tu correo electrónico"
-                name="email"
-                onChange={handleChange}
-              />
-            </Form.Group>
+          <Form.Group as={Col} controlId="email">
+            <Form.Label className="formLetraCategoria">
+              <strong>Correo electrónico *</strong>
+            </Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Ingresa tu correo electrónico"
+              name="email"
+              onChange={handleChange}
+            />
+          </Form.Group>
 
-            <Form.Group as={Col} controlId="direccion">
-              <Form.Label className="formLetraCategoria"><strong>Domicilio *</strong></Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Ingresa tu dirección"
-                name="direccion"
-                onChange={handleChange}
-              />
-            </Form.Group>
+          <Form.Group as={Col} controlId="contraseña">
+            <Form.Label className="formLetraCategoria">
+              <strong>Contraseña *</strong>
+            </Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Ingresa una contraseña"
+              name="contrasenia"
+              onChange={handleChange}
+            />
+          </Form.Group>
 
-            <Form.Group as={Col} controlId="localidad">
-              <Form.Label className="formLetraCategoria"><strong>Localidad</strong></Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Ingresa el nombre de tu ciudad"
-                onChange={handleChange}
-                name="localidad"
-              />
-            </Form.Group>
+          <Form.Group as={Col} controlId="reContraseña">
+            <Form.Label className="formLetraCategoria">
+              <strong>Repetir contaseña *</strong>
+            </Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Repita la contraseña"
+              name="reContrasenia"
+              onChange={handleChange}
+            />
+          </Form.Group>
 
-            <Form.Group as={Col} controlId="codigoPostal">
-              <Form.Label className="formLetraCategoria"><strong>Código postal</strong></Form.Label>
-              <Form.Control
-                className="col-sm-2 col-md-2 col-lg-2"
-                type="number"
-                placeholder="Ingresa tu código postal"
-                name="codigoPostal"
-                onChange={handleChange}
-              />
-            </Form.Group>
+          <Form.Group as={Col} controlId="direccion">
+            <Form.Label className="formLetraCategoria">
+              <strong>Domicilio *</strong>
+            </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ingresa tu dirección"
+              name="direccion"
+              onChange={handleChange}
+            />
+          </Form.Group>
 
-          {error ? (
-            <Alert variant={"warning"}>{mensajeError}</Alert>
-          ) : null}
+          <Form.Group as={Col} controlId="localidad">
+            <Form.Label className="formLetraCategoria">
+              <strong>Localidad</strong>
+            </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ingresa el nombre de tu ciudad"
+              onChange={handleChange}
+              name="localidad"
+            />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="codigoPostal">
+            <Form.Label className="formLetraCategoria">
+              <strong>Código postal</strong>
+            </Form.Label>
+            <Form.Control
+              className="col-sm-2 col-md-2 col-lg-2"
+              type="text"
+              placeholder="Ingresa tu código postal"
+              name="codigoPostal"
+              onChange={handleChange}
+            />
+          </Form.Group>
+
+          {error ? <Alert variant={"warning"}>{mensajeError}</Alert> : null}
           <Button variant="danger" type="submit" className="mt-5 ml-2">
             Enviar
           </Button>
